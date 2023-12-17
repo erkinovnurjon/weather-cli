c14c1e0e7c378589624148c87372acab43bc2b02
Nurjon <nurjon2477@gmail.com>
2023-12-17 16:58:17 +0500

    end project

--- index.js
+++ index.js
@@ -1,53 +1,56 @@
-import { printError , printSuccess , printHelp} from "./services/log.service.js"
+import { printError , printSuccess , printHelp, printWeather} from "./services/log.service.js"
 import getArgs from "./helpers/args.js"
 import {TOKEN_DICTIONARY, getKeyValue, saveKeyValue} from "./services/storage-save.js"
-import { getWeather } from "./services/api.service.js"
+import { getIcon, getWeather } from "./services/api.service.js"
 
-const saveToken = async (token) => {
+
+
+
+
+const saveToken = async token => {
       if (!token.length) {
             printError("Token doesn't exist")
+            return
       }
       try {
-       await saveKeyValue(TOKEN_DICTIONARY.token, token)
-        printSuccess("Token was saved")    
+            await saveKeyValue(TOKEN_DICTIONARY.token, token)
+            printSuccess('Token was saved')
       } catch (error) {
             printError(error.message)
       }
-      
 }
-const saveCity = async (city) => {
+
+const saveCity = async city => {
       if (!city.length) {
             printError("City doesn't exist")
+            return
       }
       try {
-       await saveKeyValue(TOKEN_DICTIONARY.city, city)
-        printSuccess("City was saved")    
+            await saveKeyValue(TOKEN_DICTIONARY.city, city)
+            printSuccess('City was saved')
       } catch (error) {
             printError(error.message)
       }
-      
 }
 
 const getForcast = async () => {
       try {
             const city = process.env.CITY ?? (await getKeyValue(TOKEN_DICTIONARY.city))
             const response = await getWeather(city)
-            console.log(response);
+            printWeather(response, getIcon(response.weather[0].icon))
       } catch (error) {
-         if (error?.response?.status == 404) {
-            printError("City not found")
-         } else if(error?.response?.status == 401) {
-            printError("Invalid token")
-         } else{
-            printError(error.message)
-         }
+            if (error?.response?.status == 404) {
+                  printError('City not found')
+            } else if (error?.response?.status == 401) {
+                  printError('Invalid token')
+            } else {
+                  printError(error.message)
+            }
       }
 }
 
-
-const startCli = () => {
+const startCLI = () => {
       const args = getArgs(process.argv)
-      
       if (args.h) {
             return printHelp()
       }
@@ -55,9 +58,9 @@ const startCli = () => {
             return saveCity(args.s)
       }
       if (args.t) {
-          return  saveToken(args.t)
+            return saveToken(args.t)
       }
       return getForcast()
-      
 }
-startCli()
+
+startCLI()
--- services/api.service.js
+++ services/api.service.js
@@ -3,6 +3,29 @@
 import axios from "axios"
 import { TOKEN_DICTIONARY, getKeyValue } from "./storage-save.js"
 
+const getIcon = icon => {
+      switch (icon.slice(0, -1)) {
+            case '01':
+                  return '☀️'
+            case '02':
+                  return '🌤️'
+            case '03':
+                  return '☁️'
+            case '04':
+                  return '☁️'
+            case '09':
+                  return '🌧️'
+            case '10':
+                  return '🌦️'
+            case '11':
+                  return '🌩️'
+            case '13':
+                  return '❄️'
+            case '50':
+                  return '🌫️'
+      }
+}
+
 const getWeather = async (city) => {
      
       const token = process.env.TOKEN ?? (await getKeyValue(TOKEN_DICTIONARY.token))
@@ -19,7 +42,7 @@ const getWeather = async (city) => {
             }
       })
 
-      console.log(data);
+      return data
 }
 
-export {getWeather}
+export {getWeather , getIcon}
